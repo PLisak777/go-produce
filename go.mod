68b6bae887be6b10822e3d084f479fb8e8f5fea0
Phil Lisak <phil.lisak@gmail.com>
2022-01-19 21:25:43 -0700

    Added function to create or add single or multiple items to database. Also began initial validation testing making use of govalidator by asaskevitch

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/PLisak777/go-produce
 go 1.17
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"errors"
 
 	"github.com/gorilla/mux"
+	"github.com/asaskevich/govalidator"
 )
 
 // Produce: structured list of produce items in market
@@ -16,8 +20,34 @@ type Produce struct {
 	UnitPrice   string `json:"price"`
 }
 
+// ItemValidator: Validation interface for required fields
+type ItemValidator interface {
+	Validate(r *http.Request) error
+}
+
 // Food: Array of Produce items
 var Food []Produce
+
+// We use Validate as a function to make sure user is entering required info
+// ProduceCode should be 16 chars long alphanumeric and case insensitive with dashes 		 	  separating each group of 4 chars
+// Name should be alphanumeric and case insensitive
+// UnitPrice is a number with 2 decimal places
+func (p Produce) Validate(r *http.Request) error {
+	if len(p.ProduceCode) != 19 {
+		return errors.New("Invalid Produce Code")
+	}
+	chars := []rune(p.ProduceCode)
+
+	if !govalidator.IsAlphanumeric(string(chars[0:3])) {
+		return errors.New("Invalid Produce Code")
+	}
+	return nil
+}
+
+// Interface method for item validation
+func Validate(r *http.Request, v ItemValidator) error {
+	return v.Validate(r)
+}
 
 // GetAllFoods: Displays all items in produce list
 func GetAllFoods(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +55,7 @@ func GetAllFoods(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Food)
 }
 
+// GetFoodByCode: Displays particular item by product code
 func GetFoodByCode(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("GET /produce/{code}: GetFoodByCode")
 	channelHandler := make(chan Produce)
@@ -49,6 +80,49 @@ func GetFoodByCode(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 	}
 	json.NewEncoder(w).Encode(foundItem)
+}
+
+// AddFood: Method to create and add one or more new food items to database
+func AddFood(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("POST /produce: AddFood")
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	x := bytes.TrimLeft(reqBody, " /t/r/n")
+	isArray := len(x) > 0 && x[0] == '['
+
+	itemsToAdd := make([]Produce, 0)
+	if isArray {
+		decoder := json.NewDecoder(bytes.NewBufferString(string(reqBody)))
+		err := decoder.Decode(&itemsToAdd)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		for _, item := range itemsToAdd {
+			err := Validate(r, item)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+		}
+	} else {
+		var addedItem Produce
+		json.Unmarshal(reqBody, &addedItem)
+		err := Validate(r, addedItem)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		itemsToAdd = append(itemsToAdd, addedItem)
+	}
+
+	channelHandler := make(chan []Produce)
+	go func(items []Produce) {
+		for _, item := range items {
+			Food = append(Food, item)
+		}
+		channelHandler <- items
+	}(itemsToAdd)
 }
 
 func requestHandler() {
