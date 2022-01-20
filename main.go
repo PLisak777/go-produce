f83cf3ebac801c54b20ab979e397d9b4136684ad
Phil Lisak <phil.lisak@gmail.com>
2022-01-19 19:47:03 -0700

    Added Produce struct to define types and created instantiation in main function for the Food array

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
 	"fmt"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+// Produce: structured list of produce items in market
+type Produce struct {
+	ProduceCode string
+	Name        string
+	UnitPrice   string
+}
+
+// Food: Array of Produce items
+var Food []Produce
+
+func main() {
+	// Queue up the Food array
+	Food = []Produce {
+		Produce{ProduceCode: "A12T-4GH7-QPL9-3N4M", Name: "Lettuce", UnitPrice: "3.64"},
+		Produce{ProduceCode: "E5T6-9UI3-TH15-QR88", Name: "Peach", UnitPrice: "2.99"},
+		Produce{ProduceCode: "YRT6-72AS-K736-L4AR", Name: "Green Pepper", UnitPrice: "0.79"},
+		Produce{ProduceCode: "TQ4C-VV6T-75ZX-1RMR", Name: "Gala Apple", UnitPrice: "3.59"},
+	}
+}
