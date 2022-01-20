27c2b38b7ffd8f5afcb681c3439932f3f7694c9d
Phil Lisak <phil.lisak@gmail.com>
2022-01-19 20:11:58 -0700

    Created function to lookup item by individual product code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/PLisak777/go-produce
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,20 +11,60 @@ import (
 
 // Produce: structured list of produce items in market
 type Produce struct {
-	ProduceCode string
-	Name        string
-	UnitPrice   string
+	ProduceCode string `json:"code"`
+	Name        string `json:"name"`
+	UnitPrice   string `json:"price"`
 }
 
 // Food: Array of Produce items
 var Food []Produce
 
+// GetAllFoods: Displays all items in produce list
+func GetAllFoods(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GET /produce: GetAllFoods")
+	json.NewEncoder(w).Encode(Food)
+}
+
+func GetFoodByCode(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GET /produce/{code}: GetFoodByCode")
+	channelHandler := make(chan Produce)
+
+	go func() {
+		vars := mux.Vars(r)
+		code := vars["code"]
+		var foundItem Produce
+		for index, item := range Food {
+			if item.ProduceCode == code {
+				foundItem = Food[index]
+				break
+			}
+		}
+		channelHandler <- foundItem
+	}()
+
+	foundItem := <-channelHandler
+	if foundItem.ProduceCode != "" {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	json.NewEncoder(w).Encode(foundItem)
+}
+
+func requestHandler() {
+	foodRouter := mux.NewRouter().StrictSlash(true)
+	foodRouter.HandleFunc("/produce", GetAllFoods)
+	foodRouter.HandleFunc("/produce/{code}", GetFoodByCode)
+	log.Fatal(http.ListenAndServe(":8000", foodRouter))
+}
+
 func main() {
 	// Queue up the Food array
 	Food = []Produce {
-		Produce{ProduceCode: "A12T-4GH7-QPL9-3N4M", Name: "Lettuce", UnitPrice: "3.64"},
-		Produce{ProduceCode: "E5T6-9UI3-TH15-QR88", Name: "Peach", UnitPrice: "2.99"},
-		Produce{ProduceCode: "YRT6-72AS-K736-L4AR", Name: "Green Pepper", UnitPrice: "0.79"},
-		Produce{ProduceCode: "TQ4C-VV6T-75ZX-1RMR", Name: "Gala Apple", UnitPrice: "3.59"},
+		{ProduceCode: "A12T-4GH7-QPL9-3N4M", Name: "Lettuce", UnitPrice: "3.64"},
+		{ProduceCode: "E5T6-9UI3-TH15-QR88", Name: "Peach", UnitPrice: "2.99"},
+		{ProduceCode: "YRT6-72AS-K736-L4AR", Name: "Green Pepper", UnitPrice: "0.79"},
+		{ProduceCode: "TQ4C-VV6T-75ZX-1RMR", Name: "Gala Apple", UnitPrice: "3.59"},
 	}
+	requestHandler()
 }
