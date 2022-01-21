29b5fb4c2ebd7cdd42d9ab0876eebbaf90d427de
Phil Lisak <phil.lisak@gmail.com>
2022-01-20 19:54:15 -0700

    All unit tests completed and passing

--- main_test.go
+++ main_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -19,6 +18,53 @@ func TestGetAllFoods(t *testing.T) {
 	handler.ServeHTTP(rec, req)
 
 	if status := rec.Code; status != http.StatusOK {
-		t.Error("wrong status code returned: expected %v, got %v", status, http.StatusOK)
+		t.Errorf("wrong status code returned: expected %v, got %v", status, http.StatusOK)
+	}
+}
+
+func TestGetFoodByCode(t *testing.T) {
+	req, err := http.NewRequest("GET", "/produce/ABC-DEF", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetFoodByCode)
+	handler.ServeHTTP(rec, req)
+
+	if status := rec.Code; status != http.StatusNotFound {
+		t.Errorf("wrong status code returned: expected %v, got %v", status, http.StatusNotFound)
+	}
+}
+
+func TestAddFood(t *testing.T) {
+	var payload = []byte(`[{"code":"1234-5678-AAAA-BBBB", "name":"Celery", "price":"1.99"}, {"code":"9101-1121-CCCC-DDDD", "name": "Garlic", "price": "1.65"}]`)
+
+	req, err := http.NewRequest("POST", "/groceries", bytes.NewBuffer(payload))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(AddFood)
+	handler.ServeHTTP(rec, req)
+
+	if status := rec.Code; status != http.StatusCreated {
+		t.Errorf("wrong status code returned: expected %v, got %v", status, http.StatusCreated)
+	}
+}
+
+func TestDeleteFood(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/groceries/ABC-DEF", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rec := httptest.NewRecorder()
+	handler := http.HandlerFunc(DeleteFood)
+	handler.ServeHTTP(rec, req)
+
+	if status := rec.Code; status != http.StatusNotFound {
+		t.Errorf("wrong status code returned: expected %v, got %v", status, http.StatusNotFound)
 	}
 }
